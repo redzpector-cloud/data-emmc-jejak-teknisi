60aceb1fb30eb46f157b1db074c46e21295e1737
redzpector-cloud <redzpector@gmail.com>
2026-09-09 23:53:51 +0700

    Add files via upload

--- app/src/main/java/com/jejakteknisi/gradeemmc/ScannerActivity.java
+++ app/src/main/java/com/jejakteknisi/gradeemmc/ScannerActivity.java
@@ -19,6 +19,7 @@ import androidx.core.content.ContextCompat;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.*;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
